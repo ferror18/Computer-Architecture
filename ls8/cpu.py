8439de3f9c911e0e168ccb4b2d5bdd3cca353d60
ferror18 <ferror18@gmail.com>
2020-09-16 21:31:35 -0700

    stack push and pop

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,7 +1,7 @@
 """CPU functionality."""
 
 import sys
-
+import copy
 class CPU:
     """Main CPU class."""
 
@@ -9,12 +9,17 @@ class CPU:
         """Construct a new CPU."""
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.reg[7] = 0xF4
+        self.ram[0xF4] = 0xf4
         self.pc = 0
         self.Running = True
         self.branch_table = {}
         self.branch_table[0b10000010] = self.LDI
         self.branch_table[0b00000001] = self.HLT
         self.branch_table[0b01000111] = self.PRN
+        self.branch_table[0b01000101] = self.PUSH
+        self.branch_table[0b01000110] = self.POP
+
 
     def load(self):
         """Load a program into memory."""
@@ -49,21 +54,37 @@ class CPU:
                     except ValueError:
                         print(f'Invalid number : "{n}"')
                         sys.exit(1)
-                    self.ram_write(address, n)
+                    self.write_ram(address, n)
                     address += 1
         except FileNotFoundError:
             print(f'File not found: "{sys.argv[1]}"')
             sys.exit(2)
 
-    def alu(self, op, reg_a, reg_b):
+    def ALU(self, reg_a, reg_b):
         """ALU operations."""
-
-        if op == "ADD":
+          
+        def ADD():
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
-        else:
-            raise Exception("Unsupported ALU operation")
+        def SUB():
+            self.reg[reg_a] -= self.reg[reg_b]
+        def MULT():
+            self.reg[reg_a] *= self.reg[reg_b]
+        def DIV():
+            self.reg[reg_a] /= self.reg[reg_b]
+        ALU_branch_table = {
+            0b10100001: ADD,
+            0b10100011: SUB,
+            0b10100010: MULT,
+            0b10100100: DIV,
+        }
+        try:
+            op = ALU_branch_table[self.read_ram(self.pc)]
+            op()
+        except KeyError:
+                print(f'Unsupported ALU operation: "{self.read_ram(self.pc),bin(self.read_ram(self.pc))}"')
+                sys.exit(2)  
 
+        return self.RNOP()
     def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
@@ -74,43 +95,91 @@ class CPU:
             self.pc,
             #self.fl,
             #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.read_ram(self.pc),
+            self.read_ram(self.pc + 0b1),
+            self.read_ram(self.pc + 0b10)
         ), end='')
 
-        for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+        for i in range(0b1000):
+            print(" %02X" % self.read_reg(i), end='')
 
         print()
 
     def run(self):
         """Run the CPU."""
+        verbose = True
+        if verbose == True:
+            print('Start --------------------')
+            self.trace()
         while self.Running:
-            self.trace()
-            ir = self.branch_table[self.ram_read(self.pc)]
-            
-            numofoperands = ir()
+            # self.trace()
+            try:
+                a = copy.deepcopy(self.read_ram(self.pc))
+                a &= 0b00111111
+                # print(a, bin(a))
+                if a >= 0b100000:
+                    ir = self.ALU
+                    x,y = self.read_ram(self.pc+0b1),self.read_ram(self.pc+0b10)
+                    # print(x,y)
+                    numofoperands = ir(x,y)
+                elif a < 0b100000:
+                    n = self.read_ram(self.pc)
+                    # print(n)
+                    ir = self.branch_table[n]
+                    numofoperands = ir()
 
-            self.pc += numofoperands + 0b1
-            self.trace()
+                self.pc += numofoperands + 0b1
+                if verbose == True:
+                    self.trace()
+                    if self.Running == False:
+                        print('End --------------------')
+            except KeyError:
+                print(f'Uknown Instruction: "{a,bin(a)}"')
+                sys.exit(2)
     def RNOP(self):
+        # self.pc = 0
         # print('rnop')
-        return self.ram[self.pc] >> 0b110
-    def ram_read(self, address):
+        numofoperands = self.ram[self.pc] & 0b11000000
+
+        # print(numofoperands)
+        numofoperands >>= 6
+        # print(numofoperands)
+        return numofoperands
+    def read_ram(self, address):
         return self.ram[address]
     
-    def ram_write(self, address, data):
+    def write_ram(self, address, data):
         self.ram[address] = data
+    def write_reg(self, data, regadr=0, ramadr=None):
+        if ramadr == None:
+            self.reg[regadr] = data
+        else:
+            self.reg[self.read_ram(ramadr)] = data
+    def read_reg(self, regadr=0, ramadr=None):
+        if ramadr == None:
+            return self.reg[regadr]
+        else:
+            return self.reg[self.read_ram(ramadr)]
     def LDI(self):
-        self.reg[self.ram_read(self.pc+1)] = self.ram_read(self.pc+2)
+        self.reg[self.read_ram(self.pc+1)] = self.read_ram(self.pc+2)
         return self.RNOP()
     def HLT(self):
         self.Running = False
         return self.RNOP()
     def PRN(self):
-        print(self.reg[self.ram_read(self.pc+1)])
+        print(self.read_reg(ramadr=self.pc+1))
         return self.RNOP()
-# 777
+    def PUSH(self):
+        data = self.read_reg(ramadr=self.pc+1)
+        address = self.read_reg(7) - 1
+        self.write_ram(address, data)
+        self.reg[7]-=1
+        return self.RNOP()
+    def POP(self):
+        address = self.read_reg(7)
+        data = self.read_ram(address)
+        self.write_reg(data, ramadr=self.pc+1)
+        self.reg[7]+=1
+        return self.RNOP()
 # 022
 #755
